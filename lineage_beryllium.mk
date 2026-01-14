e762a2660e5c6ac1e99d2df024b8703a9b40bdf9
FogedUser <ejazurrm@gmail.com>
2026-01-14 20:34:44 +0530

    beryllium: Adding lineage_beryllium.mk

---
+++ lineage_beryllium.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2018-2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+    BuildFingerprint=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
